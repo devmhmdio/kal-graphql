09432f789a946d48ab9b682650510f98d6bd8576
mhmd-fl <mohammed@founderandlightning.com>
2023-02-12 14:19:29 +0530

    Changed db url method

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 export abstract class Config {
-  public static mongoUrl = `mongodb://${process.env.mongo_host_name}/${process.env.mongo_db_name}?retryWrites=true&w=majority`;
+  // public static mongoUrl = `mongodb://${process.env.mongo_host_name}/${process.env.mongo_db_name}?retryWrites=true&w=majority`;
+  public static mongoUrl = `${process.env.mongo_whole_url}`;
 
 }
 
