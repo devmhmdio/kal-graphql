ae468fdf028fb1f57335854e0d25eb46592b8476
mhmd-fl <mohammed@founderandlightning.com>
2023-05-02 19:37:22 +0530

    bug fixes in payment

--- src/controllers/stripe.controller.ts
+++ src/controllers/stripe.controller.ts
@@ -4,17 +4,19 @@ const Users = require('../models/users');
 export class StripeController {
   async capturePayment({ amount, email }) {
     try {
-      await stripe.paymentIntents.create({
-        amount: amount,
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount * 100,
         currency: 'usd',
-        automatic_payment_methods: { enabled: true },
+        payment_method_types: ['card'],
       });
       await Users.findOneAndUpdate(
         { email: email },
         { $set: { balance: amount } },
         { new: true, runValidators: true }
       );
-      return 'Payment Captured Successfully';
+      return {
+        message: 'Payment Captured Successfully',
+      };
     } catch (e) {
       throw new Error(e.message);
     }
